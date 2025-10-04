4b3a1d659ad660f9f9b0be8f709429cd871ee8c5
Alain Calzadilla <alaincalzadilla@Alains-Mac-mini.local>
2025-10-03 21:11:05 -0400

    Add main project, verifier module using Webflux + RSocket (Reactive APIs), and holder script (empty)

---
+++ verifier/src/main/java/com/assessment/spruceid/verifier/VerifierApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.spruceid.verifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerifierApplication.class, args);
+    }
+
+}
